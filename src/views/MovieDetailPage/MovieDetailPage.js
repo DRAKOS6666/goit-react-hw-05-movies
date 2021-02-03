1ce3c2006c0961c817e47d51c1de46d993befe60
DRAKOS <drakostius2@gmail.com>
2021-02-03 20:12:28 +0200

    add in commit views folder

---
+++ src/views/MovieDetailPage/MovieDetailPage.js
@@ -0,0 +1,113 @@
+import { useState, useEffect, Suspense, lazy } from 'react';
+import {
+  Route,
+  NavLink,
+  useParams,
+  useRouteMatch,
+  useLocation,
+  useHistory,
+} from 'react-router-dom';
+import { fetchMovieID } from '../../service/fetchMovies';
+
+import LoaderWithTitle from '../../components/Loader/LoaderWithTitle/LoaderWithTitle';
+import style from './MovieDetailPage.module.css';
+
+const MovieDescription = lazy(() =>
+  import('../../components/MovieDescription/MovieDescription.js'),
+);
+const Cast = lazy(() =>
+  import('../../components/Cast/Cast.js' /* webpackChunkName: "Cast" */),
+);
+const Reviews = lazy(() =>
+  import(
+    '../../components/Reviews/Reviews.js' /* webpackChunkName: "Reviews" */
+  ),
+);
+
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
+
+const MovieDetailPage = () => {
+  const [movie, setMovie] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+
+  const history = useHistory();
+  const { movieId } = useParams();
+  const { path, url } = useRouteMatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    setStatus(Status.PENDING);
+    fetchMovieID(movieId)
+      .then(async res => {
+        setMovie(res);
+        await setStatus(Status.RESOLVED);
+      })
+      .catch(() => setStatus(Status.REJECTED));
+  }, [movieId]);
+
+  const handleButton = () => {
+    history.push(location?.state?.from ?? '/');
+  };
+  return (
+    <>
+      <button className={style.goBackBtn} type="button" onClick={handleButton}>
+        Go Back
+      </button>
+      {status === 'pending' && <LoaderWithTitle />}
+
+      {status === 'resolved' && (
+        <>
+          <Suspense fallback={<LoaderWithTitle />}>
+            {movie && <MovieDescription movie={movie} />}
+          </Suspense>
+          {movie && (
+            <div>
+              <h3>Addition Information</h3>
+              <NavLink
+                activeStyle={{ color: 'red' }}
+                className={style.NavLink}
+                to={{
+                  pathname: `${url}/cast`,
+                  state: { from: location.state.from },
+                }}
+              >
+                Cast
+              </NavLink>
+
+              <NavLink
+                activeStyle={{ color: 'red' }}
+                className={style.NavLink}
+                to={{
+                  pathname: `${url}/reviews`,
+                  state: { from: location.state.from },
+                }}
+              >
+                Rewiews
+              </NavLink>
+
+              <Suspense fallback={<LoaderWithTitle />}>
+                <Route path={`${path}/cast`}>
+                  <Cast />
+                </Route>
+                <Route path={`${path}/reviews`}>
+                  <Reviews />
+                </Route>
+              </Suspense>
+            </div>
+          )}
+        </>
+      )}
+
+      {status === 'rejected' && (
+        <h2>An error occurred during the download. Try again please.</h2>
+      )}
+    </>
+  );
+};
+
+export default MovieDetailPage;
---
+++ src/views/MoviesPage/MoviesPage.js
@@ -0,0 +1,116 @@
+import { useEffect, useState, lazy } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import Searchbar from '../../components/Searchbar/Searchbar';
+import { fetchMoviesQuery } from '../../service/fetchMovies';
+
+import LoaderWithTitle from '../../components/Loader/LoaderWithTitle/LoaderWithTitle';
+import { toast } from 'react-toastify';
+import style from './MoviesPage.module.css';
+
+const MovieList = lazy(() =>
+  import(
+    '../../components/MovieList/MovieList.js' /* webpackChunkName: "MoviesPageMovieList" */
+  ),
+);
+
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
+
+const MoviesPage = () => {
+  const [findedMovies, setFindedMovies] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [status, setStatus] = useState(Status.IDLE);
+
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    const queryFinal = new URLSearchParams(location.search).get('query');
+    const pageFromURL = new URLSearchParams(location.search).get('page');
+    if (queryFinal) {
+      setSearchQuery(queryFinal);
+    }
+    if (pageFromURL) {
+      setPage(Number(pageFromURL));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (page > 1) {
+      fetchMoviesQuery(query, page).then(res => {
+        setFindedMovies(prevState => [...prevState, ...res.results]);
+      });
+    }
+  }, [page]);
+
+  useEffect(() => {
+    if (query !== '') {
+      history.push({
+        ...location,
+        search: `query=${query}&page=${page}`,
+      });
+    }
+  }, [query, page]);
+
+  const moreMovies = e => {
+    setPage(page => page + 1);
+  };
+
+  const setSearchQuery = text => {
+    setPage(1);
+    setQuery(text);
+    setStatus(Status.PENDING);
+    fetchMoviesQuery(text, 1)
+      .then(res => {
+        if (res.results.length > 0) {
+          setFindedMovies(res.results);
+          setStatus(Status.RESOLVED);
+        } else {
+          setStatus(Status.IDLE);
+          toast.error('Nothing Founded. Try another query query', {
+            position: 'top-right',
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        }
+      })
+      .catch(() => {
+        setStatus(Status.REJECTED);
+      });
+  };
+
+  return (
+    <>
+      <Searchbar onSubmit={setSearchQuery} />
+      {status === 'idle' && (
+        <h1 className={style.title}>Please enter a search query.</h1>
+      )}
+      {status === 'pending' && <LoaderWithTitle />}
+      {status === 'resolved' && (
+        <>
+          <MovieList movies={findedMovies} />
+          <button type="button" onClick={moreMovies} className={style.moreBtn}>
+            More
+          </button>
+        </>
+      )}
+      {status === 'rejected' && (
+        <h1 className={style.errorTitle}>
+          An error has occurred. Try again...
+        </h1>
+      )}
+    </>
+  );
+};
+
+export default MoviesPage;
